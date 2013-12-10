6b2cdf6eda16be69aa4c114c4e8b713f43df3cc2
Hawk Weisman <hawk.weisman@gmail.com>
2013-12-10 11:24:21 -0500

    modify MARSFont.h
    - remove definitions for letters
    + add externs

--- src/MARSFont.h
+++ src/MARSFont.h
@@ -1,304 +1,45 @@
 /*
  * MARSFont.h
  * Contains definitions for the MARSFont
- * Some code in this file was generated automatically by a Python script.
- * (yes, I am that lazy)
- *
- * The rest is by Hawk Weisman
+ * by Hawk Weisman
  */
 
 #pragma		once
 #include	"MIPSCodeGen.h"
 
-typedef struct letterform {
+typedef struct {
 	int letter;
 	int moveCount;
 	statement actions[7]; 	// chosen by fair dice roll
-} letterform;				// guaranteed to be random
+} symbol;				// guaranteed to be random
 
 
-letterform setupLineOne {
-	.letter = '',
-	.moveCount = 1,
-	.actions[0] = {.identity = RMOVETO, .x = 5, .y = 5},
-}	
-
-letterform whitespace {
-	.letter = ' ',
-	.movecount = 1,
-	.actions[0] = {.identity RMOVETO, .x = 25, .y = 0},
-}
-
-//========= BEGIN PROGRAMMATICALLY-GENERATED CODE =========//
-
-letterform A {
-	.letter = 'A',
-	.moveCount = 6,
-	.actions[0] = {.identity = RMOVETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RLINETO, .x = 25, .y = -50},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 50},
-	.actions[3] = {.identity = RMOVETO, .x = -12, .y = -25},
-	.actions[4] = {.identity = RLINETO, .x = -25, .y = 0},
-	.actions[5] = {.identity = RMOVETO, .x = 50, .y = -25},
-}
-
-letterform B {
-	.letter = 'B',
-	.moveCount = 7,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RMOVETO, .x = 0, .y = -50},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 12},
-	.actions[3] = {.identity = RLINETO, .x = -25, .y = 12},
-	.actions[4] = {.identity = RLINETO, .x = 25, .y = 12},
-	.actions[5] = {.identity = RLINETO, .x = -25, .y = 12},
-	.actions[6] = {.identity = RMOVETO, .x = 25, .y = -50},
-}
-
-letterform C {
-	.letter = 'C',
-	.moveCount = 4,
-	.actions[0] = {.identity = RMOVETO, .x = 30, .y = 0},
-	.actions[1] = {.identity = RLINETO, .x = -25, .y = 25},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 25},
-	.actions[3] = {.identity = RMOVETO, .x = 0, .y = -50},
-}
-
-letterform D {
-	.letter = 'D',
-	.moveCount = 6,
-	.actions[0] = {.identity = RMOVETO, .x = 5, .y = 0},
-	.actions[1] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = -25},
-	.actions[3] = {.identity = RLINETO, .x = -25, .y = -25},
-	.actions[4] = {.identity = RMOVETO, .x = 25, .y = 0},
-	.actions[5] = {.identity = RMOVETO, .x = 5, .y = 0},
-}
-
-letterform E {
-	.letter = 'E',
-	.moveCount = 7,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[2] = {.identity = RMOVETO, .x = -25, .y = -25},
-	.actions[3] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[4] = {.identity = RMOVETO, .x = -25, .y = -25},
-	.actions[5] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[6] = {.identity = RMOVETO, .x = 5, .y = 0},
-}
-
-letterform F {
-	.letter = 'F',
-	.moveCount = 6,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RMOVETO, .x = 0, .y = -25},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[3] = {.identity = RMOVETO, .x = -25, .y = -25},
-	.actions[4] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[5] = {.identity = RMOVETO, .x = 5, .y = 0},
-}
-
-letterform G {
-	.letter = 'G',
-	.moveCount = 6,
-	.actions[0] = {.identity = RMOVETO, .x = 30, .y = 0},
-	.actions[1] = {.identity = RLINETO, .x = -25, .y = 25},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 25},
-	.actions[3] = {.identity = RLINETO, .x = 0, .y = -25},
-	.actions[4] = {.identity = RLINETO, .x = -5, .y = 0},
-	.actions[5] = {.identity = RMOVETO, .x = 10, .y = -25},
-}
-
-letterform H {
-	.letter = 'H',
-	.moveCount = 6,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RMOVETO, .x = 0, .y = -25},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[3] = {.identity = RMOVETO, .x = 0, .y = -25},
-	.actions[4] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[5] = {.identity = RMOVETO, .x = 5, .y = -50},
-}
-
-letterform I {
-	.letter = 'I',
-	.moveCount = 6,
-	.actions[0] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[1] = {.identity = RMOVETO, .x = -12, .y = 0},
-	.actions[2] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[3] = {.identity = RMOVETO, .x = -12, .y = 0},
-	.actions[4] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[5] = {.identity = RMOVETO, .x = 5, .y = -50},
-}
-
-letterform L {
-	.letter = 'L',
-	.moveCount = 5,
-	.actions[0] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[1] = {.identity = RLINETO, .x = 0, .y = 25},
-	.actions[2] = {.identity = RLINETO, .x = -12, .y = 25},
-	.actions[3] = {.identity = RLINETO, .x = -12, .y = 0},
-	.actions[4] = {.identity = RMOVETO, .x = 30, .y = -50},
-}
-
-letterform K {
-	.letter = 'K',
-	.moveCount = 6,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RMOVETO, .x = 0, .y = -25},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = -25},
-	.actions[3] = {.identity = RMOVETO, .x = -25, .y = 25},
-	.actions[4] = {.identity = RLINETO, .x = 25, .y = 25},
-	.actions[5] = {.identity = RMOVETO, .x = 5, .y = -50},
-}
-
-letterform L {
-	.letter = 'L',
-	.moveCount = 3,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[2] = {.identity = RMOVETO, .x = 5, .y = -50},
-}
-
-letterform M {
-	.letter = 'M',
-	.moveCount = 6,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RMOVETO, .x = 0, .y = -50},
-	.actions[2] = {.identity = RLINETO, .x = 12, .y = 50},
-	.actions[3] = {.identity = RLINETO, .x = 12, .y = -50},
-	.actions[4] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[5] = {.identity = RMOVETO, .x = 5, .y = -50},
-}
-
-letterform N {
-	.letter = 'N',
-	.moveCount = 5,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RMOVETO, .x = 0, .y = -50},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 50},
-	.actions[3] = {.identity = RLINETO, .x = 0, .y = -50},
-	.actions[4] = {.identity = RMOVETO, .x = 5, .y = 0},
-}
-
-letterform O {
-	.letter = 'O',
-	.moveCount = 5,
-	.actions[0] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[1] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[2] = {.identity = RLINETO, .x = -25, .y = 0},
-	.actions[3] = {.identity = RLINETO, .x = 0, .y = -50},
-	.actions[4] = {.identity = RMOVETO, .x = 30, .y = 0},
-}
-
-letterform P {
-	.letter = 'P',
-	.moveCount = 5,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RMOVETO, .x = 0, .y = -50},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 12},
-	.actions[3] = {.identity = RLINETO, .x = -25, .y = 12},
-	.actions[4] = {.identity = RMOVETO, .x = 30, .y = -25},
-}
-
-letterform Q {
-	.letter = 'Q',
-	.moveCount = 7,
-	.actions[0] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[1] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[2] = {.identity = RLINETO, .x = -25, .y = 0},
-	.actions[3] = {.identity = RLINETO, .x = 0, .y = -50},
-	.actions[4] = {.identity = RMOVETO, .x = 20, .y = 45},
-	.actions[5] = {.identity = RLINETO, .x = 10, .y = 10},
-	.actions[6] = {.identity = RMOVETO, .x = 0, .y = -55},
-}
-
-letterform R {
-	.letter = 'R',
-	.moveCount = 6,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[1] = {.identity = RMOVETO, .x = 0, .y = -50},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 12},
-	.actions[3] = {.identity = RLINETO, .x = -25, .y = 12},
-	.actions[4] = {.identity = RLINETO, .x = 25, .y = 25},
-	.actions[5] = {.identity = RMOVETO, .x = 5, .y = -50},
-}
-
-letterform S {
-	.letter = 'S',
-	.moveCount = 5,
-	.actions[0] = {.identity = RMOVETO, .x = 25, .y = 0},
-	.actions[1] = {.identity = RLINETO, .x = -25, .y = 25},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[3] = {.identity = RLINETO, .x = -25, .y = 25},
-	.actions[4] = {.identity = RMOVETO, .x = 30, .y = -50},
-}
-
-letterform T {
-	.letter = 'T',
-	.moveCount = 4,
-	.actions[0] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[1] = {.identity = RMOVETO, .x = -12, .y = 0},
-	.actions[2] = {.identity = RLINETO, .x = 0, .y = 50},
-	.actions[3] = {.identity = RMOVETO, .x = 17, .y = -50},
-}
-
-letterform U {
-	.letter = 'U',
-	.moveCount = 5,
-	.actions[0] = {.identity = RLINETO, .x = 0, .y = 35},
-	.actions[1] = {.identity = RLINETO, .x = 12, .y = 15},
-	.actions[2] = {.identity = RLINETO, .x = 12, .y = -15},
-	.actions[3] = {.identity = RLINETO, .x = 0, .y = -35},
-	.actions[4] = {.identity = RMOVETO, .x = 5, .y = 0},
-}
-
-letterform V {
-	.letter = 'V',
-	.moveCount = 3,
-	.actions[0] = {.identity = RLINETO, .x = 12, .y = 50},
-	.actions[1] = {.identity = RLINETO, .x = 12, .y = -50},
-	.actions[2] = {.identity = RMOVETO, .x = 5, .y = 0},
-}
-
-letterform W {
-	.letter = 'W',
-	.moveCount = 5,
-	.actions[0] = {.identity = RLINETO, .x = 12, .y = 50},
-	.actions[1] = {.identity = RLINETO, .x = 12, .y = -50},
-	.actions[2] = {.identity = RLINETO, .x = 12, .y = 50},
-	.actions[3] = {.identity = RLINETO, .x = 12, .y = -50},
-	.actions[4] = {.identity = RMOVETO, .x = 5, .y = 0},
-}
-
-letterform X {
-	.letter = 'X',
-	.moveCount = 4,
-	.actions[0] = {.identity = RLINETO, .x = 25, .y = 50},
-	.actions[1] = {.identity = RMOVETO, .x = -25, .y = 0},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = -50},
-	.actions[3] = {.identity = RMOVETO, .x = 5, .y = 0},
-}
-
-letterform Y {
-	.letter = 'Y',
-	.moveCount = 5,
-	.actions[0] = {.identity = RLINETO, .x = 12, .y = 25},
-	.actions[1] = {.identity = RLINETO, .x = 0, .y = 25},
-	.actions[2] = {.identity = RMOVETO, .x = 0, .y = -25},
-	.actions[3] = {.identity = RLINETO, .x = 12, .y = -25},
-	.actions[4] = {.identity = RMOVETO, .x = 5, .y = 0},
-}
-
-letterform Z {
-	.letter = 'Z',
-	.moveCount = 3,
-	.actions[0] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[1] = {.identity = RLINETO, .x = -25, .y = 50},
-	.actions[2] = {.identity = RLINETO, .x = 25, .y = 0},
-	.actions[3] = {.identity = RMOVETO, .x = 5, .y = -50},
-}
-
-//========= END PROGRAMMATICALLY-GENERATED CODE =========//
-
-letterform alphabet[26] = { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z };
-
+extern symbol A;
+extern symbol B;
+extern symbol C;
+extern symbol D;
+extern symbol E;
+extern symbol F;
+extern symbol G;
+extern symbol H;
+extern symbol I;
+extern symbol J;
+extern symbol K;
+extern symbol L;
+extern symbol M;
+extern symbol N;
+extern symbol O;
+extern symbol P;
+extern symbol Q;
+extern symbol R;
+extern symbol S;
+extern symbol T;
+extern symbol U;
+extern symbol V;
+extern symbol W;
+extern symbol X;
+extern symbol Y;
+extern symbol Z;
+extern symbol setupLineOne;
+extern symbol whitespace;
+extern symbol alphabet[26];
