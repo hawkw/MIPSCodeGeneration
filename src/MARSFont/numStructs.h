7febf257b63350ad1e1398608d8a9641b2c77f36
Hawk Weisman <hawk.weisman@gmail.com>
2013-12-10 14:02:01 -0500

    add structs for numbers

---
+++ src/MARSFont/numStructs.h
@@ -0,0 +1,125 @@
+//========= BEGIN PROGRAMMATICALLY-GENERATED CODE =========//
+letterform  = {
+	.letter = '',
+	.moveCount = 1,
+	.actions[0] = {.identity = RMOVETO, .x = 5, .y = 5},
+}
+
+letterform 0 = {
+	.letter = '0',
+	.moveCount = 7,
+	.actions[0] = {.identity = RMOVETO, .x = 0, .y = 50},
+	.actions[1] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[2] = {.identity = RLINETO, .x = 0, .y = -50},
+	.actions[3] = {.identity = RLINETO, .x = -25, .y = 0},
+	.actions[4] = {.identity = RLINETO, .x = 0, .y = 50},
+	.actions[5] = {.identity = RLINETO, .x = 25, .y = -50},
+	.actions[6] = {.identity = RMOVETO, .x = 5, .y = 0},
+}
+
+letterform 1 = {
+	.letter = '1',
+	.moveCount = 6,
+	.actions[0] = {.identity = RMOVETO, .x = 0, .y = 5},
+	.actions[1] = {.identity = RLINETO, .x = 12, .y = -5},
+	.actions[2] = {.identity = RLINETO, .x = 0, .y = 50},
+	.actions[3] = {.identity = RMOVETO, .x = -12, .y = 0},
+	.actions[4] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[5] = {.identity = RMOVETO, .x = 5, .y = -50},
+}
+
+letterform 2 = {
+	.letter = '2',
+	.moveCount = 6,
+	.actions[0] = {.identity = RMOVETO, .x = 0, .y = 5},
+	.actions[1] = {.identity = RLINETO, .x = 5, .y = -5},
+	.actions[2] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[3] = {.identity = RLINETO, .x = -25, .y = 50},
+	.actions[4] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[5] = {.identity = RMOVETO, .x = 5, .y = -50},
+}
+
+letterform 3 = {
+	.letter = '3',
+	.moveCount = 10,
+	.actions[0] = {.identity = RMOVETO, .x = 0, .y = 5},
+	.actions[1] = {.identity = RLINETO, .x = 5, .y = -5},
+	.actions[2] = {.identity = RLINETO, .x = 20, .y = 0},
+	.actions[3] = {.identity = RLINETO, .x = 0, .y = 25},
+	.actions[4] = {.identity = RLINETO, .x = -25, .y = 0},
+	.actions[5] = {.identity = RMOVETO, .x = 25, .y = 0},
+	.actions[6] = {.identity = RLINETO, .x = 0, .y = 25},
+	.actions[7] = {.identity = RLINETO, .x = -20, .y = 0},
+	.actions[8] = {.identity = RLINETO, .x = -5, .y = -5},
+	.actions[9] = {.identity = RMOVETO, .x = 30, .y = -45},
+}
+
+letterform 4 = {
+	.letter = '4',
+	.moveCount = 6,
+	.actions[0] = {.identity = RMOVETO, .x = 25, .y = 0},
+	.actions[1] = {.identity = RLINETO, .x = 0, .y = 50},
+	.actions[2] = {.identity = RMOVETO, .x = 0, .y = -25},
+	.actions[3] = {.identity = RLINETO, .x = -25, .y = 0},
+	.actions[4] = {.identity = RLINETO, .x = 25, .y = -25},
+	.actions[5] = {.identity = RMOVETO, .x = 5, .y = 0},
+}
+
+letterform 5 = {
+	.letter = '5',
+	.moveCount = 8,
+	.actions[0] = {.identity = RMOVETO, .x = 25, .y = 0},
+	.actions[1] = {.identity = RLINETO, .x = -25, .y = 0},
+	.actions[2] = {.identity = RLINETO, .x = 0, .y = 25},
+	.actions[3] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[4] = {.identity = RLINETO, .x = 0, .y = 20},
+	.actions[5] = {.identity = RLINETO, .x = -5, .y = 5},
+	.actions[6] = {.identity = RLINETO, .x = -20, .y = 0},
+	.actions[7] = {.identity = RMOVETO, .x = 30, .y = -50},
+}
+
+letterform 6 = {
+	.letter = '6',
+	.moveCount = 7,
+	.actions[0] = {.identity = RMOVETO, .x = 25, .y = 0},
+	.actions[1] = {.identity = RLINETO, .x = -25, .y = 25},
+	.actions[2] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[3] = {.identity = RLINETO, .x = 0, .y = 25},
+	.actions[4] = {.identity = RLINETO, .x = -25, .y = 0},
+	.actions[5] = {.identity = RLINETO, .x = 0, .y = -25},
+	.actions[6] = {.identity = RMOVETO, .x = 30, .y = -25},
+}
+
+letterform 7 = {
+	.letter = '7',
+	.moveCount = 5,
+	.actions[0] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[1] = {.identity = RLINETO, .x = -25, .y = 50},
+	.actions[2] = {.identity = RMOVETO, .x = 5, .y = -25},
+	.actions[3] = {.identity = RLINETO, .x = 12, .y = 0},
+	.actions[4] = {.identity = RMOVETO, .x = 12, .y = -25},
+}
+
+letterform 8 = {
+	.letter = '8',
+	.moveCount = 7,
+	.actions[0] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[1] = {.identity = RLINETO, .x = 0, .y = 50},
+	.actions[2] = {.identity = RLINETO, .x = -25, .y = 0},
+	.actions[3] = {.identity = RLINETO, .x = 0, .y = -50},
+	.actions[4] = {.identity = RMOVETO, .x = 0, .y = 25},
+	.actions[5] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[6] = {.identity = RMOVETO, .x = 5, .y = -25},
+}
+
+letterform 9 = {
+	.letter = '9',
+	.moveCount = 6,
+	.actions[0] = {.identity = RLINETO, .x = 25, .y = 0},
+	.actions[1] = {.identity = RLINETO, .x = 0, .y = 25},
+	.actions[2] = {.identity = RLINETO, .x = -25, .y = 0},
+	.actions[3] = {.identity = RLINETO, .x = 0, .y = -25},
+	.actions[4] = {.identity = RMOVETO, .x = 25, .y = 25},
+	.actions[5] = {.identity = RLINETO, .x = -25, .y = 25},
+	.actions[6] = {.identity = RMOVETO, .x = 30, .y = -50},
+}
